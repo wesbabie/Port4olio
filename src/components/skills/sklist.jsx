ad16c20ac41e4a2890e5bd1d017d4c792bc5ef84
wesbabie <wesodu448@gmail.com>
2023-02-27 09:49:59 +0100

    init commit

--- src/components/skills/sklist.jsx
+++ src/components/skills/sklist.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Sklist(props) {
   return (
     <>
-      <div className="border rounded-3xl single_card transition ease-in-out delay-300  hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300 ...">
+      <div className="border rounded-3xl single_card ">
         <div className="">
           <i className={props.logo}></i>
           <h3>{props.heading}</h3>
@@ -13,3 +13,5 @@ export default function Sklist(props) {
     </>
   );
 }
+
+// transition ease-in-out delay-300  hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300 ...
